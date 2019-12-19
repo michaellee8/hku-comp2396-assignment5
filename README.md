41ae0dc6ae16c64734e4c6a426bbc2bbac0ff5fd
michaellee8 <ckmichael8@gmail.com>
2019-12-19 17:36:48 +0800

    feat: implement some state logic and update document

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 This game follows a client-server architecture.
 
 ### Server
-The server component of this game act as a logic 
+The server component of this game act as a controller of the client. It accepts a `GameAction`, 
+which includes the a string action name and a string payload from the client, respond to the 
+action, and then update the `GameState` accordingly, populate it back to the client. 
+ 
+### Client
+The client component of this game act as a renderer of the server. It receives `GameState` from 
+server and render it accordingly, and sends back `GameAction` to the server upon user interaction 
+or client events.
--- src/com/example/hku/comp2396/assignment5/GameState.java
+++ src/com/example/hku/comp2396/assignment5/GameState.java
@@ -1,26 +1,76 @@
 package com.example.hku.comp2396.assignment5;
 
 
-public class GameState {
+import java.io.Serializable;
 
-  enum Symbol {
+public class GameState implements Serializable {
+
+  enum PlayerSymbol {
     O,
     X,
     E
   }
 
-  Symbol[][] board = {
-      {Symbol.E, Symbol.E, Symbol.E},
-      {Symbol.E, Symbol.E, Symbol.E},
-      {Symbol.E, Symbol.E, Symbol.E}
+  PlayerSymbol[][] board = {
+      {PlayerSymbol.E, PlayerSymbol.E, PlayerSymbol.E},
+      {PlayerSymbol.E, PlayerSymbol.E, PlayerSymbol.E},
+      {PlayerSymbol.E, PlayerSymbol.E, PlayerSymbol.E}
   };
 
-  Symbol winner = Symbol.E;
+  PlayerSymbol winner = PlayerSymbol.E;
 
-  Symbol nextStep = Symbol.O;
+  PlayerSymbol nextStep = PlayerSymbol.O;
 
   String OName = "";
 
   String XName = "";
+
+  String OMessage = "Enter your player name...";
+
+  String XMessage = "Enter your player name...";
+
+  String OTitle = "Tic Tac Toe";
+
+  String XTitle = "Tic Tac Toe";
+
+  /**
+   * The message to be displayed to the user as an alert before game ends for
+   * any reason. After displaying such alert, the client should close itself,
+   * and the server should reset the state.
+   */
+  String endGameMessage = "";
+
+  String getName(PlayerSymbol ps) {
+    assert ps == PlayerSymbol.O || ps == PlayerSymbol.X;
+    if (ps == PlayerSymbol.O) {
+      return OName;
+    }
+    if (ps == PlayerSymbol.X) {
+      return XName;
+    }
+    return "";
+  }
+
+  String getMessage(PlayerSymbol ps) {
+    assert ps == PlayerSymbol.O || ps == PlayerSymbol.X;
+    if (ps == PlayerSymbol.O) {
+      return OMessage;
+    }
+    if (ps == PlayerSymbol.X) {
+      return XMessage;
+    }
+    return "";
+  }
+
+  String getTitle(PlayerSymbol ps) {
+    assert ps == PlayerSymbol.O || ps == PlayerSymbol.X;
+    if (ps == PlayerSymbol.O) {
+      return OTitle;
+    }
+    if (ps == PlayerSymbol.X) {
+      return XTitle;
+    }
+    return "";
+  }
 
 }
